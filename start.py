8a80fc4cf7c513d4c8ae445041d09ecdf395da4a
joelczk <42378287+joelczk@users.noreply.github.com>
2019-09-24 13:47:02 +0800

    Update start.py

--- start.py
+++ start.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/index.html")
+def index():
+    return render_template('index.html')
+
 @app.route("/")
 def home():
     return render_template('index.html')
@@ -10,5 +14,18 @@ def home():
 def login():
     return render_template('login.html')
 
+@app.route("/register.html")
+def register():
+    return render_template('register.html')
+
+@app.route("/tutor_login.html")
+def tutor_login():
+    return render_template('tutor_login.html')
+    
+@app.route("/student_login.html")
+def student_login():
+    return render_template('student_login.html')
+
+  
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug = True, host = 'localhost', port = 5000)
