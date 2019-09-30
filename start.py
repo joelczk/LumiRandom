6affabb49d04ce219f1de79d8c77406155566e9e
joelczk <42378287+joelczk@users.noreply.github.com>
2019-09-30 11:26:00 +0800

    Update start.py

--- start.py
+++ start.py
@@ -1,31 +1,91 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import login_user, LoginManager, UserMixin, logout_user, login_required, current_user
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from sqlalchemy.orm import sessionmaker
+
+isstudent = False
+istutor = False
 
 app = Flask(__name__)
 
-@app.route("/index.html")
-def index():
-    return render_template('index.html')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{username}:{password}@{host}:{port}/{database}'\
+    .format(
+        username='<Your username>',
+        password='<Your password>',
+        host='localhost',
+        port=5432,
+        database='<Your database>'
+    )
+
+app.secret_key = "something only you know"
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+database = SQLAlchemy(app)
+
+database.init_app(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class User(UserMixin, database.Model):
+    __tablename__ = "users"
+
+    # sid = database.Column(database.String(128), primary_key = True, nullable = False)
+    # name = database.Column(database.String(128), nullable = False)
+    # password = database.Column(database.String(128), nullable = False)
+    username = database.Column(database.String(128), primary_key = True, nullable = False)
+    password = database.Column(database.String(128), nullable = False)
+
+    def get_password(self, password):
+        return self.password
+
+    def get_id(self):
+        return self.username
+
+@login_manager.user_loader
+def load_username(username):
+    return User.query.filter_by(username=username).first()
+
+def load_password(password):
+    return User.query.filter_by(password = password).first()
+
+def load_role(role):
+    return User.query.filter_by(role = role).first()
 
 @app.route("/")
 def home():
     return render_template('index.html')
-
-@app.route("/login.html")
-def login():
-    return render_template('login.html')
-
-@app.route("/register.html")
-def register():
-    return render_template('register.html')
-
-@app.route("/tutor_login.html")
-def tutor_login():
-    return render_template('tutor_login.html')
-    
-@app.route("/student_login.html")
-def student_login():
-    return render_template('student_login.html')
-
   
+@app.route("/login/", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html", error=False)
+
+    # user = str(request.form['username'])
+    # psasword = str(request.form['password'])
+    user = load_username(request.form["username"])
+    password = load_password(request.form["password"])
+
+    if user is None:
+        return render_template("login.html", error = True)
+    else:
+        if password is None:
+            return render_template("login.html", error = True)
+        else:
+            login_user(user)
+    login_user(user)
+    print('logged in')
+    return redirect(url_for('home'))
+
+@app.route("/logout/")
+@login_required
+def logout():
+    logout_user()
+    print('logout')
+    return redirect(url_for('home'))
+
 if __name__ == "__main__":
     app.run(debug = True, host = 'localhost', port = 5000)
