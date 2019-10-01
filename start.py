58334f5f444f4c0885f8385f728d76a2297155f8
joelczk <42378287+joelczk@users.noreply.github.com>
2019-10-01 08:36:29 +0800

    Update start.py

--- start.py
+++ start.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import login_user, LoginManager, UserMixin, logout_user, login_required, current_user
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, Form, TextField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from sqlalchemy.orm import sessionmaker
 
@@ -14,11 +14,11 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{username}:{password}@{host}:{port}/{database}'\
     .format(
-        username='<Your username>',
-        password='<Your password>',
+        username='<username>',
+        password='<password>',
         host='localhost',
         port=5432,
-        database='<Your database>'
+        database='<database>'
     )
 
 app.secret_key = "something only you know"
@@ -33,12 +33,10 @@ login_manager.init_app(app)
 class User(UserMixin, database.Model):
     __tablename__ = "users"
 
-    # sid = database.Column(database.String(128), primary_key = True, nullable = False)
-    # name = database.Column(database.String(128), nullable = False)
-    # password = database.Column(database.String(128), nullable = False)
     username = database.Column(database.String(128), primary_key = True, nullable = False)
     password = database.Column(database.String(128), nullable = False)
-
+    role = database.Column(database.String(128), nullable = False)
+    name = database.Column(database.String(128), nullable = False)
     def get_password(self, password):
         return self.password
 
@@ -52,17 +50,17 @@ def load_username(username):
 def load_password(password):
     return User.query.filter_by(password = password).first()
 
-def load_role(role):
-    return User.query.filter_by(role = role).first()
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    return render_template("register.html")
-  
+# @app.route("/register/", methods=["GET", "POST"])
+# def register():
+#     if request.method == "POST":
+#         return (render_template("register.html"))
+#     if request.method == "GET":
+#         return render_template("register.html")
 @app.route("/login/", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
@@ -83,6 +81,32 @@ def login():
     login_user(user)
     print('logged in')
     return redirect(url_for('home'))
+
+@app.route("/register/", methods=["GET", "POST", "PUT"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html", error=False)
+
+    user = (request.form["username"])
+    password = (request.form["password"])
+    name = (request.form["name"])
+    role = (request.form["role"])
+    user_check = User.query.filter_by(username=user).first()
+    name_check = User.query.filter_by(name = name).first()
+    role_check = str(role).lower()
+    if user_check is None and name_check is None and (role_check == 'student' or role_check == 'tutor'):
+        register = User(name = name, username = user, password = password, role = role)
+        database.session.add(register)
+        database.session.commit()
+        return redirect(url_for('login'))
+    else:
+        if user_check is not None:
+            return render_template("register.html", error1 = True)
+        if name_check is not None:
+            return render_template("register.html", error2 = True)
+        if role_check != 'student' or role_check != 'tutor':
+            return render_template("register.html", error3 = True)
+    return render_template("register.html")
 
 @app.route("/logout/")
 @login_required
