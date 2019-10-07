02e129716ea0362a64c1428e8a01ecb6da75b79d
Ice Clementi <42372756+iceclementi@users.noreply.github.com>
2019-10-07 11:06:46 +0800

    Update routes.py

--- LumiRandom/lumirandom/routes.py
+++ LumiRandom/lumirandom/routes.py
@@ -1,30 +1,13 @@
 from flask import render_template, url_for, flash, redirect, request, abort
 from lumirandom import app, db, bcrypt
 from lumirandom.forms import RegistrationForm, LoginForm
-from lumirandom.models import User, Post, Courses, Students
-from sqlalchemy_utils.functions import sort_query
+from lumirandom.models import User, Post, Students, SAccount, Courses, TakenCourses, TakingCourses, Professors, PAccount
 from flask_login import login_user, current_user, logout_user, login_required
-
-
-posts = [
-    {
-        'author': 'A',
-        'title': 'Forum Post 1',
-        'content': 'First post content',
-        'date_posted': '3 October 2019'
-    },
-    {
-        'author': 'B',
-        'title': 'Forum Post 2',
-        'content': 'Second post content',
-        'date_posted': '3 October 2019'
-    }
-]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', posts=posts)
+    return render_template('home.html')
 
 
 @app.route("/about")
@@ -38,7 +21,7 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(name=form.f_name.data + ' ' + form.l_name.data, account_id=form.account_id.data, password=hashed_password)
+        user = SAccount(name=form.f_name.data + ' ' + form.l_name.data, account_id=form.account_id.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
         flash(f'Account created for {form.f_name.data} {form.l_name.data}!', 'success')
@@ -52,7 +35,7 @@ def login():
         return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(account_id=form.account_id.data).first()
+        user = SAccount.query.filter_by(account_id=form.account_id.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
@@ -65,7 +48,9 @@ def login():
 
 @app.route("/logout")
 def logout():
-    logout_user()
+    if current_user.is_authenticated:
+        logout_user()
+        flash(f'Logout Successful! Please Visit Again!', 'success')
     return redirect(url_for('home'))
 
 
@@ -75,22 +60,60 @@ def account():
     return render_template('account.html', title='Account')
 
 
+@app.route("/mymodules")
+@login_required
+def modules():
+    takingmods = TakingCourses.query.filter_by(sid=current_user.sid)
+    takenmods = TakenCourses.query.filter_by(sid=current_user.sid).order_by(TakenCourses.year.desc(), TakenCourses.sem.desc())
+    mods = Courses()
+    profs = Professors()
+    return render_template('mymodules.html', title='My Modules', takingmods=takingmods, takenmods=takenmods, mods=mods, profs=profs)
+
+
 @app.route("/module-search", methods=['GET', 'POST'])
 @login_required
 def module_search():
     page = request.args.get('page', 1, type=int)
-    courses = Courses.query.order_by(Courses.cid.asc(),Courses.cname.asc()).paginate(page=page, per_page=15)
+    courses = Courses.query.order_by(Courses.cid.asc()).paginate(page=page, per_page=15)
     return render_template('module_search.html', title='Module Search', courses=courses)
+
+
+
+@app.route("/module/<string:cid>", methods=['GET', 'POST'])
+@login_required
+def module(cid):
+    module = Courses.query.filter_by(cid=cid).first()
+    if TakenCourses.query.filter_by(sid=current_user.sid, cid=cid).first():
+        status = "taken"
+    elif TakingCourses.query.filter_by(sid=current_user.sid, cid=cid).first():
+        status = "taking"
+    else:
+        status = "nil"
+    return render_template('module.html', title=cid, module=module, status=status)
+
+
+@app.route("/module/<string:cid>/enrol", methods=['GET', 'POST'])
+@login_required
+def module_enrol(cid):
+    if TakenCourses.query.filter_by(sid=current_user.sid, cid=cid).first():
+        redirect(url_for('module', cid=cid))
+    elif Professors.query.filter_by(cid=cid).first() == None:
+        redirect(url_for('module', cid=cid))
+    else:
+        course = TakingCourses(sid=current_user.sid, cid=cid)
+        db.session.add(course)
+        db.session.commit()
+    return redirect(url_for('module', cid=cid))
+
 
 @app.route("/students", methods=['GET', 'POST'])
 @login_required
 def students():
     page = request.args.get('page', 1, type=int)
-
     students = Students.query.order_by(Students.year.asc(), Students.name.asc()).paginate(page=page, per_page=15)
     return render_template('student.html', title='Student List', students=students)
 
 @app.route("/tutors")
 @login_required
 def staff():
-    return render_template("staff.html")
+    return render_template("staff.html", title='Staff')
